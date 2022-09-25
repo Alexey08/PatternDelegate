66e71f4d5c9ca6311018beca4fdc4a9929d50b33
Alexey08 <bauer080@gmail.com>
2022-09-25 21:36:29 +0500

    Initial Commit

---
+++ PatternDelegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PatternDelegate
+//
+//  Created by Алексей Уланов on 25.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
